6edf4fb7b9ea323947c644b418587223b8cdfb69
Johannes Löhnn <jlohnn@gmail.com>
2014-11-28 11:55:03 +0100

    Okay, so testing works at least, don't really know how to fully use it yet though.

--- __tests__/spec/jesttest-spec.js
+++ __tests__/spec/jesttest-spec.js
@@ -6,24 +6,10 @@
 var path = '../../src/components/jesttest.js';
 
 jest.dontMock(path);
-describe('CheckboxWithLabel', function () {
-    it('changes the text after click', function () {
-        var React = require('react/addons');
-        var CheckboxWithLabel = require(path);
-        var TestUtils = React.addons.TestUtils;
 
-        // Render a checkbox with label in the document
-        var checkbox = TestUtils.renderIntoDocument();
-
-        // Verify that it's Off by default
-        var label = TestUtils.findRenderedDOMComponentWithTag(
-            checkbox, 'label');
-        expect(label.getDOMNode().textContent).toEqual('Off');
-
-        // Simulate a click and verify that it is now On
-        var input = TestUtils.findRenderedDOMComponentWithTag(
-            checkbox, 'input');
-        TestUtils.Simulate.change(input);
-        expect(label.getDOMNode().textContent).toEqual('On');
+describe("sum", function () {
+    it('adds 1 + 2 to equal 3', function () {
+        var sum = require(path);
+        expect(sum(1, 2)).toBe(3);
     });
 });
--- src/components/jesttest.js
+++ src/components/jesttest.js
@@ -3,16 +3,8 @@
  */
 
 /** @jsx React.DOM */
-var React = require('react/addons');
-var CheckboxWithLabel = React.createClass({
-        getInitialState: function() {
-            return { isChecked: false };
-        },
-        onChange: function() {
-            this.setState({isChecked: !this.state.isChecked});
-        },
-        render: function() {
-            return (this.state.isChecked ? this.props.labelOn : this.props.labelOff);
+
+function sum(value1, value2) {
+    return value1 + value2;
 }
-});
-module.exports = CheckboxWithLabel;
+module.exports = sum;
